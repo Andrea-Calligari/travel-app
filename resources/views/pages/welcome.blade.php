ba47ee9dfdf1a2a983bb2ba2e7a44d4901e8ad48
Andrea Calligari <153660323+Andrea-Calligari@users.noreply.github.com>
2024-08-14 12:33:46 +0200

    Initial commit

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('title', 'Welcome')
+
+<h1 class="text-danger text-center py-5">Welcome to your Template Repository!!!</h1>
+<p class="text-center fs-3"> All front-end assets is set and ready for your new projects! Enjoy! </p>
+
+
+
+@section('content')
+
+
+@endsection
