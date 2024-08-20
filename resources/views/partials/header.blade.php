fd197d748836b4770cfb639658f303de646eee08
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-08-20 17:53:14 +0200

    ADDED: navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,16 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <div class="d-flex align-items-center">
-
-                <h1><a href="{{route('home')}}">&#128640; Tavelers</a></h1>
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="d-flex justify-content-between w-100">
+            <div class="col-4">
+                <h1><a href="{{route('home')}}">🚀 Travelers</a></h1>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
-                  
+                    <span class="navbar-toggler-icon"></span>
                 </button>
+            </div>
+            <div class="col-3">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -17,15 +19,14 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('trips.index')}}">I Miei Viaggi</a>
                         </li>
-                        <li>
+                        <li class="nav-item">
                             <a class="nav-link" href="{{route('trips.create')}}">Crea un nuovo viaggio</a>
-
                         </li>
-
                     </ul>
                 </div>
-
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
+
 </header>
